94f721e79abd0101de7103fcffd8bbe78628f24f
Akhil Sadam <sadam.akhil@gmail.com>
2021-07-08 23:16:53 -0500

    Update Bash

---
+++ README.md
@@ -0,0 +1,2 @@
+If you are a member of the UTKL Group, please visit the install directory, which contains notes on how to setup ROOT and Geant4 for a complete group.
+The other scripts are to do with logging in/out of TACC and running the PlasticPET simulation remotely.
--- init/login.sh
+++ init/login.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # A script to run PlasticPET on stampede2: login copies remote and runs remote.
+# The SCP will only need to be run the first time, and the corresponding clones in remote.sh can then be commented out.
 cd bash
+git add .
 git commit -a -m "Update Bash"
 git push origin master
 scp bash/init/remote.sh as_tacc@stampede2.tacc.utexas.edu:/work2/07752/as_tacc/Desktop/PlasticPET/bash/init/remote.sh
